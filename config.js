aa785839108a1cdab8c6117b1f95929f00095e36
trungkenny12345 <85819951+trungkenny12345@users.noreply.github.com>
2021-06-13 13:42:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại không yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    reply: 'Giờ yêu rồi nè :3',
+    mess: 'Anh biết mà :>>>>. Yêu em nhiều nhiều :>>>',
     messDesc: 'Book cái lịch hẹn nhé em.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/kenny.trung.080704/' //link mess của các bạn. VD: https://m.me/nam.nodemy
