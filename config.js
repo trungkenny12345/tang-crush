b7a7692a5d896535bf0c73c50d4cc2aa4dc202f3
trungkenny12345 <85819951+trungkenny12345@users.noreply.github.com>
2021-06-13 13:35:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Em à!',
+    introDesc: `Vườn anh trồng chủ yếu là hoa,
+    thấy em cười, à hóa là yêu`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -15,5 +13,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/kenny.trung.080704/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
