fe5b1c07d74971058411e7b3075c36f46129cc7b
trungkenny12345 <85819951+trungkenny12345@users.noreply.github.com>
2021-06-13 13:39:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Book cái lịch hẹn nhé em.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/kenny.trung.080704/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
